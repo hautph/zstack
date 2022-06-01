e19d32524f4c71ebe6de115fc355be278ad0ca79
Trần Phúc Hậu <hautp@gocit.vn>
2022-06-01 14:14:14 +0700

    Update agent.sh

--- agent/agent.sh
+++ agent/agent.sh
@@ -6,60 +6,60 @@ DEB_PACKAGE_NAME="qemu-guest-agent"
 DISTRO=$(cat /etc/*-release | grep -w PRETTY_NAME | cut -d= -f2 | tr -d '"')
 
  if cat /etc/*release | grep ^NAME | grep CentOS; then
-    echo "==============================================="
+    echo "======================================================================="
     echo "Installing packages $YUM_PACKAGE_NAME on $DISTRO"
-    echo "==============================================="
+    echo "======================================================================="
     yum install -y $YUM_PACKAGE_NAME
     systemctl start qemu-guest-agent
     systemctl enable qemu-guest-agent
     /bin/bash -c "$(curl -s -S http://169.254.169.254/vm-tools.sh)"
  elif cat /etc/*release | grep ^NAME | grep Red; then
-    echo "==============================================="
+    echo "======================================================================="
     echo "Installing packages $YUM_PACKAGE_NAME on $DISTRO"
-    echo "==============================================="
+    echo "======================================================================="
     yum install -y $YUM_PACKAGE_NAME
     systemctl start qemu-guest-agent
     systemctl enable qemu-guest-agent
     /bin/bash -c "$(curl -s -S http://169.254.169.254/vm-tools.sh)"
  elif cat /etc/*release | grep ^NAME | grep Fedora; then
-    echo "================================================"
+    echo "======================================================================="
     echo "Installing packages $YUM_PACKAGE_NAME on $DISTRO"
-    echo "================================================"
+    echo "======================================================================="
     yum install -y $YUM_PACKAGE_NAME
     systemctl start qemu-guest-agent
     systemctl enable qemu-guest-agent
     /bin/bash -c "$(curl -s -S http://169.254.169.254/vm-tools.sh)"
  elif cat /etc/*release | grep ^NAME | grep Ubuntu; then
-    echo "==============================================="
+    echo "======================================================================="
     echo "Installing packages $DEB_PACKAGE_NAME on $DISTRO"
-    echo "==============================================="
+    echo "======================================================================="
     apt-get update
     apt-get install -y $DEB_PACKAGE_NAME
     systemctl start qemu-guest-agent
     systemctl enable qemu-guest-agent
     /bin/bash -c "$(curl -s -S http://169.254.169.254/vm-tools.sh)"
  elif cat /etc/*release | grep ^NAME | grep Debian ; then
-    echo "==============================================="
+    echo "======================================================================="
     echo "Installing packages $DEB_PACKAGE_NAME on $DISTRO"
-    echo "==============================================="
+    echo "======================================================================="
     apt-get update
     apt-get install -y $DEB_PACKAGE_NAME
     systemctl start qemu-guest-agent
     systemctl enable qemu-guest-agent
     /bin/bash -c "$(curl -s -S http://169.254.169.254/vm-tools.sh)"
  elif cat /etc/*release | grep ^NAME | grep Mint ; then
-    echo "============================================="
+    echo "======================================================================="
     echo "Installing packages $DEB_PACKAGE_NAME on $DISTRO"
-    echo "============================================="
+    echo "======================================================================="
     apt-get update
     apt-get install -y $DEB_PACKAGE_NAME
     systemctl start qemu-guest-agent
     systemctl enable qemu-guest-agent
     /bin/bash -c "$(curl -s -S http://169.254.169.254/vm-tools.sh)"
  elif cat /etc/*release | grep ^NAME | grep Knoppix ; then
-    echo "================================================="
+    echo "======================================================================="
     echo "Installing packages $DEB_PACKAGE_NAME on $DISTRO"
-    echo "================================================="
+    echo "======================================================================="
     apt-get update
     apt-get install -y $DEB_PACKAGE_NAME
     systemctl start qemu-guest-agent
